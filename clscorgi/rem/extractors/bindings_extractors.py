8302ddce9c12603faa70fb35e94a35b4d5e4339d
Lukas Plank <lupl@tuta.io>
2024-02-21 17:07:13 +0100

    Implemented BindingsExtractor for ReM corpus.

--- clscorgi/rem/extractors/bindings_extractors.py
+++ clscorgi/rem/extractors/bindings_extractors.py
@@ -1,8 +1,20 @@
 """ReM bindings extractors"""
 
+from collections.abc import Callable
 from pathlib import Path
-from clscorgi.bindings_abc import BindingsExtractor
 
+from lxml import etree
+from toolz import valmap
+
+from clscorgi.bindings_abc import BindingsExtractor
+from clscorgi.rem.extractors.tree_extractors import (
+    get_id,
+    get_title,
+    get_genre,
+    get_token,
+    get_publication,
+    get_source
+)
 
 class ReMBindingsExtractor(BindingsExtractor):
     def __init__(self, header_path: Path):
@@ -10,4 +22,16 @@ class ReMBindingsExtractor(BindingsExtractor):
         super().__init__()
 
     def generate_bindings(self) -> dict:
-        pass
+        tree = etree.parse(self.header_path)
+
+        _bindings_mapping: dict[str, Callable] = {
+            "id": get_id,
+            "title": get_title,
+            "genre": get_genre,
+            "token_count": get_token,
+            "publication": get_publication,
+            "soure": get_source
+        }
+
+        bindings = valmap(lambda x: x(tree), _bindings_mapping)
+        return bindings
