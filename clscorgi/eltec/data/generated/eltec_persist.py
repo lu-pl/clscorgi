4ea4c2d44b19da4e36f6a6430e3b4552ef3061b6
Lukas Plank <lupl@tuta.io>
2024-04-09 12:38:48 +0200

    Fix #2; persisted ELTeC data as JSON

---
+++ clscorgi/eltec/data/generated/eltec_persist.py
@@ -0,0 +1,25 @@
+"""Persist ELTeC data as JSON for further prossing."""
+
+import json
+
+from collections.abc import Iterator
+
+from importlib.resources import files
+
+from clscorgi.eltec.extractors.bindings_extractor import ELTeCBindingsExtractor
+from clscorgi.eltec.extractors.link_extractor import get_eltec_xml_links
+from clscorgi.eltec.runner import _REPOS
+
+
+if __name__ == "__main__":
+    for repo in _REPOS:
+        uris: Iterator[str] = get_eltec_xml_links(repos=[repo])
+        data_file = files("clscorgi.eltec.data.generated") / f"{repo.lower()}.json"
+
+        bindings = [
+            dict(ELTeCBindingsExtractor(uri))
+            for uri in uris
+        ]
+
+        with open(data_file, "w") as f:
+            json.dump(bindings, f, indent=4)
