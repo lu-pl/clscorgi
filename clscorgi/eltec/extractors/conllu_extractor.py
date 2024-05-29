b0f46aafc17c854b602ef96295a23bd9f78329bf
SteffRhes <steffrhes@pm.me>
2024-05-29 10:12:29 +0200

    add more documentation

--- clscorgi/eltec/extractors/conllu_extractor.py
+++ clscorgi/eltec/extractors/conllu_extractor.py
@@ -3,20 +3,20 @@ import os
 import sys
 
 
+# some tsv were too big, so increase max memory allocation of csv module
 csv.field_size_limit(sys.maxsize)
 
 
-def get_conllu_data(repo_id, stem):
+def get_conllu_data(repo_id: str, stem: str) -> dict:
+    """main function for conllu processing, takes repo_id and stem and returns conllu statistics"""
     
-    # returned dictionary structure
+    # returned dictionary, defined here for communication of its structure
     result_dict = {
         "token_count": None,
     }
     
     def read_file():
-        """
-        find conllu file given repo_id and stem, and return its content as a list of rows
-        """
+        """find conllu file given repo_id and stem, and return its content as a list of rows"""
         
         # find folder
         conllu_folder_path = None
@@ -35,22 +35,21 @@ def get_conllu_data(repo_id, stem):
             return list(csv.reader(f, delimiter="\t"))
                         
     def process_conllu_content(conllu_content, result_dict):
-        """
-        process conllu content
-        """
+        """process conllu content"""
         
-        # values of interest
+        # statistics of interest
         token_count = 0
         
-        # main loop over rows
+        # main loop over rows, create statistics
         for row in conllu_content:
             if len(row) != 0 and not row[0].startswith("#"):
                 token_count += 1
                 
+        # load statistics into dict, return
         result_dict["token_count"] = token_count
         return result_dict
     
+    # main calls and return
     conllu_content = read_file()
     result_dict = process_conllu_content(conllu_content, result_dict)
-    
     return result_dict
