10e120921dd00c02746f014d5ea2bc168c95f35b
Lukas Plank <lupl@tuta.io>
2024-06-06 16:07:16 +0200

    Implemented basic E13 triple generator

--- clscorgi/eltec_features/triple_generators.py
+++ clscorgi/eltec_features/triple_generators.py
@@ -1,9 +1,10 @@
 """Triple generators for ELTeC features."""
 
-from clisn import clscore, crm, crmcls
+from clisn import clscore, crm, crmcls, crmdig
 from clscorgi.models import ELTeCFeaturesBindingsModel
 from lodkit import URINamespace, mkuri_factory, ttl
-from rdflib.namespace import RDF
+from rdflib import Literal, URIRef
+from rdflib.namespace import RDF, XSD
 
 mkuri = mkuri_factory(clscore)
 
@@ -11,9 +12,32 @@ def e13_eltec_features_generator(
         bindings: ELTeCFeaturesBindingsModel,
 ):
     """Triple generator for ELTec Features triples"""
-    # mkuri(bindings.resource_uri)
+    x2_uri = mkuri(f"{bindings.resource_uri}")
 
-    return ttl(
+    e13_token_triples = ttl(
         mkuri(),
-        (RDF.type, crm.E13_Attribute_Assignment)
-    )
+        (RDF.type, crm.E13_Attribute_Assignment),
+        (crm.P16_used_specific_object, ttl(
+            mkuri("veld_data_16_eltec_conllu_stats [D1]"),
+            (RDF.type, crmdig.D1_Digital_Object),
+            (crm.P1_is_identified_by, ttl(
+                mkuri("veld_data_16_eltec_conllu_stats [E42]"),
+                (RDF.type, crm.E41_Identifier),
+                (crm.P190_has_symbolic_content,
+                 Literal("https://github.com/acdh-oeaw/veld_data_16_eltec_conllu_stats/"))
+            ))
+        )),
+        (crm.P140_assigned_attribute_to, x2_uri),
+        (crm.P177_assigned_property_of_type, crmcls.Y1_exhibits_feature),
+        (crm.P141_assigned, ttl(
+            mkuri("Token [X3]"),
+            (RDF.type, crmcls.X3_Feature),
+            (crm.P2_has_type, URIRef("https://clscor.io/entity/type/feature/token")),
+            (crm.P91i_is_unit_of, [
+                (RDF.type, crm.E54_Dimension),
+                (crm.P90_has_value,
+                 Literal(f"{bindings.conllu_stats.count_token}", datatype=XSD.integer))
+            ])
+        )))
+
+    return e13_token_triples
