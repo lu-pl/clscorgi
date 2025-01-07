7ba7dfc79e22bb6da91181be9416baca0274f879
Lukas Plank <lupl@tuta.io>
2025-01-07 12:03:18 +0100

    feat: implement Vocabs Lookup Utility class

---
+++ clscorgi/vocabs/vocab_lookup.py
@@ -0,0 +1,50 @@
+"""Generic Vocab Lookup Utility. This is intended to supersed the clscorgi.vocabs.vocabs solution."""
+
+from collections.abc import Callable
+from functools import partial
+import os
+
+from rdflib import Graph, Literal, RDFS, URIRef
+
+
+class VocabLookupException(Exception):
+    """Exception for indicating a failed vocab term lookup."""
+
+
+class Vocabs:
+    """Vocabs Lookup Utility.
+
+    The class allows to reverse-lookup subject URIs based on an rdfs:label term
+    for several graphs which can be accessed by the vocabs name.
+
+    Example:
+
+    vocabs = Vocabs(identifier=_vocabs_path / "identifier.ttl")
+    print(vocabs.identifier("textgrid"))  # https://clscor.io/entity/type/identifier/textgrid
+    """
+
+    def __init__(self, **vocabs: os.PathLike | Graph) -> None:
+        self._vocabs = vocabs
+
+        self._vocab_graphs: list[Graph] = [
+            graph if isinstance(graph, Graph) else Graph().parse(graph)
+            for _, graph in self._vocabs.items()
+        ]
+        self._lookup_map: dict[str, Callable[[str], URIRef]] = {
+            name: partial(self._lookup_term, graph=graph)
+            for name, graph in zip(self._vocabs.keys(), self._vocab_graphs)
+        }
+
+    def __getattr__(self, attr) -> URIRef:
+        return self._lookup_map[attr]
+
+    def _lookup_term(self, term: str, graph: Graph) -> URIRef:
+        """Lookup a subject URI in a Graph based on its rdfs:label term."""
+        _subjects = graph.subjects(RDFS.label, Literal(term))
+
+        try:
+            vocab_uri = next(_subjects)
+        except StopIteration:
+            raise VocabLookupException(f"No subject URI for term '{term}'.")
+
+        return vocab_uri
